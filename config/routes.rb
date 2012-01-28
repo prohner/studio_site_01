1b2ecaaf4824deb794d6824cf4ad81cb91e0f86b
Preston Rohner <prestonrohner@me.com>
2012-01-28 12:42:18 -0800

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ StudioSite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "static_pages#home"
 
   # See how all your routes lay out with "rake routes"
 
