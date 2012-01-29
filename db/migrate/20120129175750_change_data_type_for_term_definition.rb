12ff25d9d224a217635f7f5556b0da0e9ca54836
Preston Rohner <prestonrohner@me.com>
2012-01-29 10:21:57 -0800

    Organizing migrations

--- db/migrate/20120129175750_change_data_type_for_term_definition.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForTermDefinition < ActiveRecord::Migration
-  def up
-    change_column :terms, :definition, :text
-  end
-
-  def down
-    change_column :terms, :definition, :string
-  end
-end
--- db/migrate/20120129181341_change_data_type_for_term_definition.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDataTypeForTermDefinition < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
